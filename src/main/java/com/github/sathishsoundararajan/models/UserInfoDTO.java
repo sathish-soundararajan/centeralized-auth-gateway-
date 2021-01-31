7d89aeb2026f04e78dbb254bc94ed30825273af0
Sathish Soundararajan <sathish.soundararajan@voyant.com>
2021-01-31 11:48:48 +0530

    Added eureka service discovery client

--- src/main/java/com/github/sathishsoundararajan/models/UserInfoDTO.java
+++ src/main/java/com/github/sathishsoundararajan/models/UserInfoDTO.java
@@ -8,9 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserInfoDTO {
-
 	private Long userId;
 	private String userRole;
 	private String userType;
-
 }
--- src/main/java/com/github/sathishsoundararajan/service/JWTService.java
+++ src/main/java/com/github/sathishsoundararajan/service/JWTService.java
@@ -1,22 +1,14 @@
 package com.github.sathishsoundararajan.service;
 
-import static com.github.sathishsoundararajan.constants.FilterConstants.USER_ID_CLAIM;
-import static com.github.sathishsoundararajan.constants.FilterConstants.USER_ROLE_CLAIM;
-import static com.github.sathishsoundararajan.constants.FilterConstants.USER_TYPE_CLAIM;
-
 import java.io.UnsupportedEncodingException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
-
-import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+
+import org.springframework.stereotype.Service;
 
 @Service
 public class JWTService {
@@ -37,17 +29,4 @@ public class JWTService {
 		}
 		return null;
 	}
-
-	public String createToken(Long userId, String userType, String userRole) {
-		final JWTCreator.Builder jwtBuilder = JWT.create().withIssuer(ISSUER_NAME).withSubject(userId.toString())
-				.withClaim(USER_ID_CLAIM, userId).withIssuedAt(new Date()).withClaim(USER_TYPE_CLAIM, userType)
-				.withClaim(USER_ROLE_CLAIM, userRole);
-
-		Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-		calendar.add(Calendar.SECOND, 120);
-		jwtBuilder.withExpiresAt(calendar.getTime());
-		jwtBuilder.withClaim("tokenType", "accessToken");
-		return jwtBuilder.sign(algorithm);
-	}
-
 }
